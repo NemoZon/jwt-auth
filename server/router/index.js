af993db03e039cbd12f00d6d37dccec5c3be53ef
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2024-06-03 17:15:03 +0200

    fix: auth middleware path changed

--- server/router/index.js
+++ server/router/index.js
@@ -2,7 +2,7 @@ const Router = require('express').Router
 const UserController = require('../controllers/user.controller')
 const router = new Router();
 const {body} = require('express-validator')
-const authMiddleware = require('./middlewares/auth.middleware')
+const authMiddleware = require('../middlewares/auth.middleware')
 
 router.post('/registration',
     body('email').isEmail(),
